e261e524ecd001d56c69a8208b8a81a96f912fbb
1cyberlangke1 <145818885+1cyberlangke1@users.noreply.github.com>
2023-11-25 23:55:42 +0800

    头文件

---
+++ permcomb.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "permcomb.h"
+#ifndef PERCOMB_H
+#define PERCOMB_H
+
+long long P(int a, int b);
+long long C(int a, int b);
+long long fact_arr[21] = {1,1,2,6,24,120,720,5040,40320,362880,3628800,39916800,479001600,6227020800,87178291200,1307674368000,20922789888000,355687428096000,6402373705728000,121645100408832000,2432902008176640000};
+
+long long P(int n, int k) {
+	if (n < k||n>20||k>20||n<0||k<0) return -1;
+	return fact_arr[n] / fact_arr[n - k];
+}
+
+long long C(int n, int k) {
+	if (n < k||n>20||k>20||n<0||k<0) return -1;
+	return (fact_arr[n] / fact_arr[k]) / fact_arr[n - k];
+}
+
+#endif
